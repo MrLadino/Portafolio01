2d9d9059f5c8840c589f5761393ad72023a7576e
Juan José Gómez <112130665+xGomezx@users.noreply.github.com>
2024-03-01 20:32:39 -0500

    Update PictureProfile.jsx

--- src/components/PictureProfile/PictureProfile.jsx
+++ src/components/PictureProfile/PictureProfile.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import imgProfile from '../../assets/imgProfile.jpg'
+import pictureProfile from '../../assets/pictureProfile.jpg'
 import './PictureProfile.css'
 
 export const PictureProfile = () => {
   return (
     <section className='sectProfile'>
-        <img className='imgProfile' src={imgProfile} alt="ImgProfile" />
+        <img className='imgProfile' src={pictureProfile} alt="ImgProfile" />
         <h2 className='nameProfile'>Juan Jose Gomez</h2>
         <span className='borderName'></span>
     </section>
