6b88efa5ba81beb968f182fc1651bae595053897
Juan José Gómez <112130665+xGomezx@users.noreply.github.com>
2024-03-01 20:18:32 -0500

    Update MyInformation.jsx

--- src/components/MyInformation/MyInformation.jsx
+++ src/components/MyInformation/MyInformation.jsx
@@ -5,7 +5,7 @@ export const MyInformation = () => {
   return (
     <p className='information'>Estudiante apasionado por el desarrollo de software, con un fuerte anhelo por el aprendizaje continuo y la superación personal. Poseo una dedicación inquebrantable a mis objetivos, trabajando con diligencia y tesón hasta alcanzarlos. Mi fascinación por la tecnología, presente desde mi temprana juventud, me ha impulsado a adquirir sólidos conocimientos en software y hardware. <br/>
 
-Habilidades blandas:<br/>
+<b>Habilidades blandas:<b/><br/>
 
 Capacidad de comunicación excepcional, facilitando la creación de relaciones interpersonales sólidas y agradables con facilidad.<br/>
 Trabajo en equipo eficaz, colaborando de forma armoniosa y proactiva para alcanzar metas comunes.<br/>
